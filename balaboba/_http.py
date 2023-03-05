05822f07ba37283ea851d4db71e2c71f01f5a44c
monosans <hsyqixco@protonmail.com>
2023-03-05 18:44:00 +0300

    Set json parameter type hint to Any

--- balaboba/_http.py
+++ balaboba/_http.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Dict, Optional
+from typing import Any, Optional
 
 from requests import Session
 
@@ -11,9 +11,7 @@ class HTTPSession:
     def __init__(self, session: Optional[Session]) -> None:
         self.session = session
 
-    def get_response(
-        self, *, method: str, endpoint: str, json: Optional[Dict[str, Any]] = None
-    ) -> Any:
+    def get_response(self, *, method: str, endpoint: str, json: Any = None) -> Any:
         if isinstance(self.session, Session):
             return self._fetch(
                 method=method, endpoint=endpoint, json=json, session=self.session
@@ -23,14 +21,7 @@ class HTTPSession:
                 method=method, endpoint=endpoint, json=json, session=session
             )
 
-    def _fetch(
-        self,
-        *,
-        method: str,
-        endpoint: str,
-        json: Optional[Dict[str, Any]],
-        session: Session,
-    ) -> Any:
+    def _fetch(self, *, method: str, endpoint: str, json: Any, session: Session) -> Any:
         with session.request(
             method, f"https://yandex.ru/lab/api/yalm/{endpoint}", json=json
         ) as response:
